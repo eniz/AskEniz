b59898c5126ddc7d63e6243a0e0edb1fbee02a78
enzglk <enizgulek@gmail.com>
2013-01-08 22:09:00 +0200

    First Commit

---
+++ js/model/Data.js
@@ -0,0 +1,32 @@
+﻿Data =[ 
+{
+    Id: "Sakarya",
+    Name: "Sakarya",
+    Info:"Sakarya Şehri",
+    Detail: 
+        [
+            { Id: "Ogun", Name: "Ogün Altıparmak", Info: "Futbolcu" },
+            {Id:"Sapanca",Name:"Sapanca Gölü",Info:"Göl"}
+        ]
+},
+{
+    Id: "Ogun",
+    Name: "Ogün Altıparmak",
+    Info:"Futbolcu",
+    Detail: 
+        [
+            { Id: "Trabzon", Name: "Trabzon", Info: "Şehir" },
+            { Id: "Sakarya", Name: "Sakarya", Info: "Sakarya Şehri" },
+            {Id:"Trabzon Spor",Name:"Trabzon Spor",Info:"Futbol Klübü"}
+        ]
+},
+{
+    Id: "Sapanca",
+    Name: "Sapanca Gölü",
+    Info:"Göl",
+    Detail: 
+        [
+            { Id: "Sakarya", Name: "Sakarya", Info: "Sakarya Şehri" }
+        ]
+}
+]
